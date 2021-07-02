95cb8adddf3a95170ad9771419caca26df3c02ad
Milhous Zhang <milhous@sina.com>
2021-07-02 19:59:04 +0800

    U ESLint 注释

--- mage-cli/.eslintrc.js
+++ mage-cli/.eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
+    // ESLint默认使用Espree作为其解析器
     parser: '@typescript-eslint/parser',
+    /**
+     * extends属性值可以是一个字符串或字符串数组
+     * 数组中每个配置项继承它前面的配置
+     */
     extends: [
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
         'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
-        'prettier',
-    ], //使用推荐的React代码检测规范
+    ],
+    // ESLint支持使用第三方插件
     plugins: ['@typescript-eslint'],
+    // 指定环境，每个环境都有自己预定义的全局变量，可以同时指定多个环境
     env: {
         browser: true,
         node: true,
     },
+    // 支持在配置文件添加共享设置
     settings: {
         //自动发现React的版本，从而进行规范react代码
         react: {
@@ -19,14 +27,24 @@ module.exports = {
             version: 'detect',
         },
     },
+    // parser解析代码时的参数
     parserOptions: {
         //指定ESLint可以解析JSX语法
         ecmaVersion: 2020,
+        //设置为script(默认)或module（如果你的代码是ECMAScript模块)
         sourceType: 'module',
+        // 使用的额外的语言特性,所有选项默认都是false
         ecmaFeatures: {
+            //启用JSX
             jsx: true,
         },
     },
+    /**
+     * 具体规则配置
+     * off或0--关闭规则
+     * warn或1--开启规则，警告级别(不会导致程序退出)
+     * error或2--开启规则，错误级别(当被触发的时候，程序会退出)
+     */
     rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-var-requires': 0,
@@ -38,9 +56,8 @@ module.exports = {
         '@typescript-eslint/no-use-before-define': 0,
         '@typescript-eslint/class-name-casing': 0,
         '@typescript-eslint/triple-slash-reference': 0,
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-        "react/prop-types": 0,
+        'react-hooks/rules-of-hooks': 'warn',
+        'react-hooks/exhaustive-deps': 'off',
         'prettier/prettier': [
             'error',
             {
@@ -54,7 +71,7 @@ module.exports = {
                 insertPragma: false,
                 tabWidth: 2,
                 useTabs: false,
-                endOfLine: 'auto'
+                endOfLine: 'auto',
             },
         ],
     },
--- packages/librarys/broadcastChannel/workers/broadcastChannel.sw.ts
+++ packages/librarys/broadcastChannel/workers/broadcastChannel.sw.ts
@@ -1,47 +1,40 @@
-// import { CacheFirst } from 'workbox-strategies';
-// import { NetworkFirst } from 'workbox-strategies';
-// import { precacheAndRoute } from 'workbox-precaching';
-// import { registerRoute } from 'workbox-routing';
 
-// // Your other import statements go here
-// precacheAndRoute((self as any).__WB_MANIFEST);
+const initialize = (service: ServiceWorkerGlobalScope): void => {
+    // 通过 workbox 配置，指定根路径 / 下的资源是 NetworkFirst 的，意味着这部分资源都是网络优先，有新的资源文件会先请求网络，
+    // 而 / static / 路径下，因为 CRA 打包出来都使用了 revving（文件带hash，确保新文件不会被缓存），所以设置为 Cache 优先。
+    // registerRoute('/', new NetworkFirst());
 
-// const initialize = (service: ServiceWorkerGlobalScope): void => {
-//     // 通过 workbox 配置，指定根路径 / 下的资源是 NetworkFirst 的，意味着这部分资源都是网络优先，有新的资源文件会先请求网络，
-//     // 而 / static / 路径下，因为 CRA 打包出来都使用了 revving（文件带hash，确保新文件不会被缓存），所以设置为 Cache 优先。
-//     // registerRoute('/', new NetworkFirst());
+    // registerRoute(
+    //     '/static/',
+    //     new CacheFirst({
+    //         cacheName: 'static-cache',
+    //         matchOptions: {
+    //         ignoreVary: true
+    //         }
+    //     })
+    // );
 
-//     // registerRoute(
-//     //     '/static/',
-//     //     new CacheFirst({
-//     //         cacheName: 'static-cache',
-//     //         matchOptions: {
-//     //         ignoreVary: true
-//     //         }
-//     //     })
-//     // );
+    service.addEventListener('message', evt => {
+        if (!evt.data ) {
+            return;
+        }
 
-//     service.addEventListener('message', evt => {
-//         if (!evt.data ) {
-//             return;
-//         }
+        if (evt.data.type === 'SKIP_WAITING') {
+            service.skipWaiting();
+        } else {
+            service.clients.matchAll().then(function(clients) {
+                clients.forEach(function(client) {
+                client.postMessage(evt.data);
+                });
+            });
+        }
+    });
 
-//         if (evt.data.type === 'SKIP_WAITING') {
-//             service.skipWaiting();
-//         } else {
-//             service.clients.matchAll().then(function(clients) {
-//                 clients.forEach(function(client) {
-//                 client.postMessage(evt.data);
-//                 });
-//             });
-//         }
-//     });
+    // 跳过等待期
+    service.addEventListener('install', evt => evt.waitUntil(service.skipWaiting()));
 
-//     // 跳过等待期
-//     service.addEventListener('install', evt => evt.waitUntil(service.skipWaiting()));
+    // 一旦激活就开始控制任何现有客户机
+    service.addEventListener('activate', evt => evt.waitUntil(service.clients.claim()));
+}
 
-//     // 一旦激活就开始控制任何现有客户机
-//     service.addEventListener('activate', evt => evt.waitUntil(service.clients.claim()));
-// }
-
-// initialize(self as any)
+initialize(self as any)
