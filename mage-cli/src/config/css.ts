fa952d41d31f22bf59ba4c19ff9daf29293a6ecb
Milhous <milhous@sina.com>
2022-05-02 21:22:11 +0800

    A webpack config css

--- mage-cli/src/config/css.ts
+++ mage-cli/src/config/css.ts
@@ -1,3 +1,8 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import PostcssPluginPx2rem from 'postcss-plugin-px2rem';
+
+
 // 样式
 export default (args: any) => {
     return {};
