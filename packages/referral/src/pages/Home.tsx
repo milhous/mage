57d1eb96ffe4a8cdfe1b077251073e365f6dbaf5
Milhous <milhous@sina.com>
2022-06-08 21:30:38 +0800

    A reset & library css A requests xhr

--- packages/referral/src/pages/Home.tsx
+++ packages/referral/src/pages/Home.tsx
@@ -1,5 +1,13 @@
 import './Home.less';
 
 export default function Home(): JSX.Element {
-  return <div>Hello world!!!</div>;
+  return (
+    <div>
+      Hello world!!!
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </div>
+  );
 }
