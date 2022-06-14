528881c624e5ad3ce59c321326cf4765d65eefcd
Milhous <milhous@sina.com>
2022-06-14 22:06:54 +0800

    A ui dropdown

---
+++ packages/ui/src/dropdown/index.tsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useMemo, useRef, useState} from 'react';
+import Dropdown from 'rc-dropdown';
+import Menu, {Item as MenuItem} from 'rc-menu';
+import Tooltip from 'rc-tooltip';
+import {Scrollbars} from 'rc-scrollbars';
+
+import './index.less';
+
+const UIDropdown = (): JSX.Element => {
+  return <div>111</div>;
+};
