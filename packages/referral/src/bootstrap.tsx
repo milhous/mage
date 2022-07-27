b0e17a63e1da0c924996eee689c4b5a6f73dda07
Milhous <milhous@sina.com>
2022-07-27 09:14:33 +0800

    A referral init

--- packages/referral/src/bootstrap.tsx
+++ packages/referral/src/bootstrap.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 
@@ -7,9 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.Suspense fallback={null}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.Suspense>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
--- packages/referral/src/pages/Home.tsx
+++ packages/referral/src/pages/Home.tsx
@@ -1,13 +1,12 @@
+import {Outlet} from 'react-router-dom';
+
 import './Home.less';
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      Hello world!!!
-      <ul>
-        <li>1</li>
-        <li>2</li>
-      </ul>
-    </div>
+    <main className="affiliate affiliate-home">
+      Hello world!
+      <Outlet />
+    </main>
   );
 }
