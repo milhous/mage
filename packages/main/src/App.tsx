a69b1774906468d50b8a275ccee15069df4daa20
Milhous <milhous@sina.com>
2022-07-28 09:59:53 +0800

    U main app route

--- packages/main/src/App.tsx
+++ packages/main/src/App.tsx
@@ -8,13 +8,13 @@ import './App.less';
 
 import Home from './pages/Home';
 
-interface ISystem {
+interface ISystemInfo {
   remote: string;
   url: string;
   module: string;
 }
 
-const System = (props: {system: ISystem}): JSX.Element => {
+const System = (props: {system: ISystemInfo}): JSX.Element => {
   const {system} = props;
   const {remote, module, url} = system;
 
@@ -27,18 +27,43 @@ const System = (props: {system: ISystem}): JSX.Element => {
   );
 };
 
-const App = () => {
-  const [system, setSystem] = useState({
-    remote: 'affiliate',
-    url: 'https://www.local.devbitgame.com:9013/remoteEntry.js',
-    module: './App',
-  });
+/**
+ * 系统类型
+ * @property REFERRAL 邀请
+ * @property AFFILIATE 代理
+ */
+enum SystemType {
+  REFERRAL = 0,
+  AFFILIATE,
+}
 
+// 系统接口
+const SystemPorts = {
+  [SystemType.REFERRAL]: 9007,
+  [SystemType.AFFILIATE]: 9013,
+};
+
+const getSystemInfo = (type: number): ISystemInfo => {
+  const remote = SystemType[type].toLowerCase();
+  const port = SystemPorts[type];
+  const protocol = location.protocol;
+  const url = `${protocol}//www.local.devbitgame.com:${port}/remoteEntry.js`;
+  const module = './App';
+
+  return {
+    remote,
+    url,
+    module,
+  };
+};
+
+const App = () => {
   return (
     <Suspense fallback={<WidgetSpinner />}>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route path="/affiliate/*" element={<System system={system} />} />
+          <Route path="/referral/*" element={<System system={getSystemInfo(SystemType.REFERRAL)} />} />
+          <Route path="/affiliate/*" element={<System system={getSystemInfo(SystemType.AFFILIATE)} />} />
           <Route path="*" element={<Navigate replace to="/" />} />
         </Route>
       </Routes>
